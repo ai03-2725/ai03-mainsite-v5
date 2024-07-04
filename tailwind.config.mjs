d8f1df656572904287f32d1bea6b5f04d73872cf
ai03-2725 <admin@ai03.com>
2024-07-04 10:55:59 +0900

    Get rid of FOUT

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: [
-					'"Inter Variable", sans-serif',
-					{
-						// fontFeatureSettings: '"cv11", "ss01"',
-						// fontVariationSettings: '"opsz" 32'
-					},
-				],
+				sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
 			}
 		},
 	},
